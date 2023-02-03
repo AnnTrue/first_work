4dbe6123e5fecbe1c28f2f521f8f1769c52e64d2
AnnTrue <ann.doni.93@gmail.com>
2023-02-03 17:30:35 +0300

    add-validation

---
+++ form_val.js
@@ -0,0 +1,74 @@
+"use strict";
+
+let button = document.querySelector(".form__button");
+let checkbox = document.querySelector(".form__checkbox");
+
+button.classList.add("button__disabled");
+
+checkbox.onclick = function disableButton() {
+  if (checkbox.checked) {
+    button.classList.remove("button__disabled");
+    button.removeAttribute("disabled");
+  } else {
+    button.classList.add("button__disabled");
+    button.setAttribute("disabled", true);
+  }
+};
+
+button.addEventListener("click", (event) => {
+  // event.preventDefault()
+
+  function isEmailValid(value) {
+    const EMAIL_REGEXP =
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+    return EMAIL_REGEXP.test(value);
+  }
+
+  let name = document.querySelector(".form__name").value;
+  let email = document.querySelector(".form__email").value;
+  let message = document.querySelector(".form__message").value;
+
+  function isMessageValid(value) {
+    if (message.length > 10) {
+      return false;
+    }
+
+    if (true) {
+      const cyrillicPattern = /^\p{Script=Cyrillic}+$/u;
+      return cyrillicPattern.test(value);
+    }
+  }
+
+  if (name.length && isEmailValid(email) && isMessageValid(message)) {
+    button.classList.remove("button__disabled");
+    button.removeAttribute("disabled");
+  } else {
+    button.classList.add("button__disabled");
+    button.setAttribute("disabled", true);
+
+    checkbox.checked = false;
+
+    let div = document.createElement("div");
+    div.className = "alert";
+    div.innerHTML =
+      "<strong>Внимание!</strong> Вы не ввели данные или ввели их неверно.";
+
+    document.body.append(div);
+
+    let time = setTimeout(() => {
+      let modal = document.querySelector(".alert");
+      modal.remove();
+    }, 5000);
+
+    div.addEventListener("mouseover", () => {
+      clearTimeout(time);
+    });
+
+    div.addEventListener("mouseout", () => {
+      time = setTimeout(() => {
+        let modal = document.querySelector(".alert");
+        modal.remove();
+      }, 5000);
+    });
+  }
+});
--- scroll.js
+++ scroll.js
@@ -1,12 +1,53 @@
-const smoothLinks = document.querySelectorAll ('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
 
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+window.addEventListener('scroll', () => {
+    const HEADER_HEIGHT = 100;
+    let scrollDistance = window.scrollY + HEADER_HEIGHT; // растояние от верха страницы до места скролла
+
+    document.querySelectorAll('section').forEach((section, currentSection) => {
+
+        // здесь мы смотрим в какой мы секции
+        if (section.offsetTop <= scrollDistance) {
+
+            // находим все ссылки меню и проходимся по ним
+            document.querySelectorAll('.navigation__item').forEach(link => {
+                // сначала удаляем все старые классы (active)
+                link.classList.remove('navigation__item--active');
+            });
+
+            document.querySelectorAll('.navigation__item')[currentSection].classList.add('navigation__item--active');
+        }
+
+    });
+
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+        document.querySelectorAll('.navigation__item').forEach(link => {
+            // сначала удаляем все старые классы (active)
+            link.classList.remove('navigation__item--active');
+        });
+        document.getElementById('last_child').classList.add('navigation__item--active');
+    }
+
+});
+
+
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('.header').offsetHeight;
+        // const topOffset = 0; // если не нужен отступ сверху 
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
         });
     });
-};
+});
